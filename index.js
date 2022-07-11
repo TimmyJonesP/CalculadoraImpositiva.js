b838a637028dcd154e33e278e5c710904307f0b2
Sandro <sandrocarioli43@gmail.com>
2022-07-11 19:09:28 -0300

    /Post, crash

--- index.js
+++ index.js
@@ -3,53 +3,10 @@ const productos = [];
 let tabla;
 let textoImpuestos;
 let textoTotal;
-
-/// funcion con variable de nombre
-let neim = prompt("Cómo te llamas?")
-function nombr(neim){
-    
-    if (neim == "" || neim === null){
-        alert("Por favor ingrese su nombre!")
-		neim = prompt("Ingrese nombre")
-        nombr()
-    }
-
-    else{
-        edad()
-    }
-
-    return neim
-}
-
-///funcion que verifica edad mediante if y en caso de estar en la correspondiente dará con la funcion anterior.
-function edad(){
-    let years = parseInt(prompt("Que edad tenés " + neim +  "? Ingresa número."))
-    if (years >= 120 ){
-        alert("Estás muy viejo :(")
-        edad()
-    } 
-    
-    else if (years < 18){
-        alert("Todavía sos muy joven")
-        edad()
-    }
-    
-    else if (years > 18){
-        alert(neim + ", calculemos el coste total de tus juegos.")
-        registrarProductos()
-
-    }
-    
-    else{
-        alert("Por favor " + neim + " ingrese su edad para poder continuar")
-        edad()
-    }
-}
-
 ///OBJETO
 class Producto {
     constructor(nombre, valor, impuestos, total) {
-        this.nombre = nombre.toUpperCase();
+        this.nombre = nombre;
         this.valor = valor;
         this.impuestos = impuestos;
         this.total = total;
@@ -66,23 +23,14 @@ function inicializarElementos() {
     textoTotal = document.querySelector("#totalTotal span");
 }
 
-///Entrada de Objetos mediante prompt
-function registrarProductos() {
-    let numeroProductos = parseInt(prompt("Cuantos juegos quiere calcular?"));
-    for (let index = 0; index < numeroProductos; index++) {
-        let nombre = prompt("Ingrese el nombre del juego para poder identificarlo en la tabla");
-        if(nombre == null || nombre === ""){
-            alert("Ingrese nombre por favor!")
-            nombre = prompt("Ingrese el nombre del juego para poder identificarlo en la tabla")
-        }
-        
-        let valor = parseFloat(prompt("Ingrese el precio que indica steam"));
-        if(valor === null || valor === 0 || valor === NaN || valor === "0"){
-            alert("Ingrese valor por favor!")
-            valor = parseFloat(prompt("Ingrese el precio que indica steam"))
-        }
+///Entrada de Objetos
+let Agregar = document.querySelector("#agregar");
 
+Agregar = function(){
+    e.preventDefault()
 
+        let nombre = document.getElementById("name").value;
+        let valor = parseInt(document.getElementById("precio").value);
         let impuestos = valor * 0.65;
         let total = valor + impuestos;
 
@@ -91,11 +39,9 @@ function registrarProductos() {
             valor,
             impuestos,
             total
-    );
+        );
     productos.push(juegoARegistrar);
-  }
 }
-
 ///Agregar objetos a la tabla.
 function agregarProductosTabla() {
   productos.forEach((producto) => {
@@ -108,7 +54,8 @@ function agregarProductosTabla() {
     tabla.tBodies[0].append(filaTabla);
   });
 }
-
+  
+  
 ///Totales para visualización del usuario
 function calcularTotales() {
     let totalImpuestos = 0;
