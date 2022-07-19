a2233ac61311857b822e8cc50e2b3e1c36c4f56d
Sandro <sandrocarioli43@gmail.com>
2022-07-19 14:52:44 -0300

    :/

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # JS-CH-37470
-El array que es para el desafío complementario está establecido al final de la cadena con un console.log que lleva la lista de Objetos. En el archivo JS se puede encontrar al final.
+Hola Alex, estuve craneando un montón la forma de poder hacer el Localstorage útil dentro de mi proyecto, pero tengo un problema con el array donde se ubican los productos que mi idea era que al entrar este se llene de los datos del local storage que estan almacenados con el JSON.parse para que sea el objeto, pero el Array.reduce me aparece cómo anónimo. En sí, no puedo cargar completamente los datos del local para hacer un push decente D:
+
+Intenté también pushear los objetos almacenados como string en el local para generar nuevos Productos y que estos si puedan aparecer pero por lo visto tengo que cambiar toda la sintaxis de mi proyecto para poder utilizarlo de esa manera.
+
+Intenté muchisimas cosas pero no hice pushes porque no los pude hacer funcionar de la manera que quería. Puedo almacenar los datos de la forma ordenada dentro del localstorage pero no puedo pushearlos y que sean parte del array original como quiero que funcione.
--- index.js
+++ index.js
@@ -1,20 +1,8 @@
 //Constantes y var
-const productos = [];
+const juegos = [];
 let tabla;
 let textoTotal;
 let textoImpuestos;
-///OBJETO
-class Producto {
-    constructor(nombre, valor, impuestos, total) {
-        this.nombre = nombre;
-        this.valor = valor;
-        this.impuestos = impuestos;
-        this.total = total;
-    }
-    ///Totales reducidos
-    calcularImpuestos = () => this.impuestos;
-    calcularTotal = () => this.total;
-}
 
 ///Entrada a Tabla
 function inicializarElementos() {
@@ -23,66 +11,95 @@ function inicializarElementos() {
     textoTotal = document.querySelector("#totalTotal span");
 }
 
-///Agregar objetos a la tabla.
-function agregarProductosTabla() {
-    productos.forEach((producto) => {
-        let filaTabla = document.createElement("tr");
-        filaTabla.innerHTML = `
-        <td>${producto.nombre}</td>
-        <td>${producto.valor}</td>
-        <td>${producto.impuestos}</td>
-        <td>${producto.total}</td>`;
-        tabla.tBodies[0].append(filaTabla);
-    });
-    calcularTotales()
+///OBJETO
+class Producto {
+    constructor(nombre, valor, impuestos, total) {
+        this.nombre = nombre;
+        this.valor = valor;
+        this.impuestos = impuestos;
+        this.total = total;
+    }
+    ///Totales comprimidos BIG TEXT
+    calcularImpuestos = () => this.impuestos;
+    calcularTotal = () => this.total;
 }
+
 ///funcion para limpiar la tabla e imprimir bien el array
 function limpiarTabla() {
     while (tabla.rows.length > 1) {
         tabla.deleteRow(1);
     }
 }
-///Totales para visualización del usuario
-function calcularTotales() {
-    let totalImpuestos = 0;
-    let totalTotal = 0;
 
-    totalImpuestos = productos.reduce(
-        (acumulador, item) => acumulador + item.calcularImpuestos(), 0
-    );
-
-    totalTotal = productos.reduce(
-        (acumulador, item) => acumulador + item.calcularTotal(), 0
-    );
-
-    textoImpuestos.innerText = totalImpuestos;
-    textoTotal.innerText = totalTotal;
-}
 ///Entrada de Objetos
 let entry = document.getElementById("entry");
 entry.addEventListener("click", registro);
 function registro(e) {
     e.preventDefault()
-
+    
     let nombre = document.getElementById("name").value;
     let valor = parseInt(document.getElementById("precio").value);
     let impuestos = valor * 0.65;
     let total = valor + impuestos;
-
+    
     let juegoARegistrar = new Producto(
         nombre,
         valor,
         impuestos,
         total
-    );
-    productos.push(juegoARegistrar);
-    limpiarTabla()
-    agregarProductosTabla()
+        );
+        juegos.push(juegoARegistrar);
+        limpiarTabla();
+        agregarProductosTabla();
+        localStorage.setItem("cloud", JSON.stringify(juegos))
+    }
+    ///Totales para visualización del usuario
+    function calcularTotales() {
+        let totalImpuestos = 0;
+        let totalTotal = 0;
+        
+        totalImpuestos = juegos.reduce(
+            (acumulador, item) => acumulador + item.calcularImpuestos(), 0
+            );
+            
+            totalTotal = juegos.reduce(
+                (acumulador, item) => acumulador + item.calcularTotal(), 0
+                );
+                
+                textoImpuestos.innerText = totalImpuestos;
+    textoTotal.innerText = totalTotal;
+}
+///Agregar objetos a la tabla.
+function agregarProductosTabla() {
+    juegos.forEach((juego) => {
+        let filaTabla = document.createElement("tr");
+        filaTabla.innerHTML = `
+        <td>${juego.nombre}</td>
+        <td>${juego.valor}</td>
+        <td>${juego.impuestos}</td>
+        <td>${juego.total}</td>`;
+        tabla.tBodies[0].append(filaTabla);
+    });
+    calcularTotales()
 }
 /// Secuencia
 function main() {
     inicializarElementos();
-    calcularTotales()
+    recuperar()
 }
 ///inicio de ciclo.
 main()
+function recuperar(){ 
+    let nube = JSON.parse(localStorage.getItem("cloud"))
+    nombre = nube.nombre
+    valor = nube.valor
+    impuestos = nube.impuestos
+    total = nube.total
+    let recuperarData = new Producto(
+        nombre,
+        valor,
+        impuestos,
+        total
+    )
+    juegos.push(recuperarData)
+}
