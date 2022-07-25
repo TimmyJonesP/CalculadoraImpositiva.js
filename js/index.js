d27d55ab06bc12252cd931fc8be5c8353541ed0b
Sandro <sandrocarioli43@gmail.com>
2022-07-25 14:30:15 -0300

    Agregados botones e interacción con framework.

--- js/index.js
+++ js/index.js
@@ -38,7 +38,7 @@ entry.addEventListener("click", registro);
 function registro(e) {
     e.preventDefault()
     
-    localStorage.clear
+    localStorage.clear()
     let nombre = document.getElementById("name").value;
     let valor = parseInt(document.getElementById("precio").value);
     let impuestos = valor * 0.75;
@@ -53,12 +53,51 @@ function registro(e) {
     juegos.push(juegoARegistrar);
     limpiarTabla();
     agregarProductosTabla();
+    ///guardado en local
     localStorage.setItem("nombre", JSON.stringify(nombre))
     localStorage.setItem("valor", JSON.stringify(valor))
     localStorage.setItem("impuestos", JSON.stringify(impuestos))
     localStorage.setItem("total", JSON.stringify(total))
     localStorage.setItem("cloud", JSON.stringify(juegos))
+    // toast
+    Toastify({
+        text: "Se ha almacenado el último juego y se ha aplicado a la tabla.",
+        className: "info",
+        gravity: "bottom",
+        position: "right",
+        style: {
+          background: "linear-gradient(to left,#49a09d,#5f2c82)",
+        }
+      }).showToast();
 }
+
+///botón de eliminar
+let limpiar = document.getElementById("cleanse");
+limpiar.addEventListener("click", limpio);
+function limpio(e){
+    e.preventDefault()
+    ///Limpio localstorage y la clase producto.
+    localStorage.clear()
+        while(juegos.length > 0)
+    juegos.pop();
+    ///while con pop para eliminar del último al primero, similar al ejecutado en la próxima función.
+    limpiarTabla()
+    ///para que los números queden en 0.
+    calcularTotales()
+    ///para vaciar el historial si se cumple la condición.
+    validacion()
+    ///una tostadita porque queda lindo.
+    Toastify({
+        text: "Se han eliminado todos los datos y restablecido la página.",
+        className: "info",
+        gravity: "bottom",
+        position: "right",
+        style: {
+          background: "linear-gradient(90deg, rgba(190,127,163,1) 0%, rgba(253,29,29,1) 51%, rgba(252,176,69,1) 100%)",
+        }
+      }).showToast();
+}
+
 ///Totales para visualización del usuario
 function calcularTotales() {
     let totalImpuestos = 0;
@@ -94,14 +133,14 @@ function restaurar(){
     let valor = JSON.parse(localStorage.getItem("valor"))
     let impuestos = JSON.parse(localStorage.getItem("impuestos"))
     let total = JSON.parse(localStorage.getItem("total"))
-    historial.innerHTML ="Tu última búsqueda fué: " + nombre +"<br>" + "Indicado en la plataforma: $" +  valor + "<br>" + " Impuestos: $" + impuestos + "<br>" + "TOTAL: $" + total
+    historial.innerHTML = "Tu última búsqueda fué: " + nombre +"<br>" + "Indicado en la plataforma: $" +  valor + "<br>" + " Impuestos: $" + impuestos + "<br>" + "TOTAL: $" + total
     console.log(JSON.parse(localStorage.getItem("cloud")))
 }
 
 ///OPERADORES
 function validacion(){
     cookies = localStorage.length
-    cookies > 0 ? restaurar() : null
+    cookies > 0 ? restaurar() : historial.innerHTML = ""
 }
 
 /// Secuencia
